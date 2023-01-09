9a85a901641ee006ea44057d43b2237b937afc64
chostler24 <connor.hostler@gmail.com>
2023-01-09 14:56:46 -0600

    Fixed typo

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     resolve({
       status: 200,
-      body: 'Success',
+      body: 'success',
     });
     console.log('Got a response from the API');
     reject(new Error(''));
