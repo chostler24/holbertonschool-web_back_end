f6d932e0002e28fba7190e1d9b2f9bd9bca46b1e
chostler24 <connor.hostler@gmail.com>
2023-01-11 10:44:51 -0600

    Adding task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else {
+      this._amount = newAmount;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    } else {
+      this._currency = newCurrency;
+    }
+  }
+
+  displayFullPrice() {
+    return (`${this.amount} ${this.currency.displayFullCurrency()}`);
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
