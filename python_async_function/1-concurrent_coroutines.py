aca4f69ef041b1b920ab2b228c01b95a8b72c735
chostler24 <connor.hostler@gmail.com>
2023-05-08 15:51:14 -0500

    Adding task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Asynchronously spawns `wait_random` `n` times with a specified `max_delay`.
+Args:
+    n (int): The number of times to call `wait_random`.
+    max_delay (int): The maximum delay for each call to `wait_random`.
+Returns:
+    List[float]: A list of the delays (float values) in ascending order.
+"""
+import asyncio
+from typing import List
+from random import random
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """wait_n function declaration"""
+    delays = []
+    tasks = []
+    for i in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+
+    for task in tasks:
+        delay = await task
+        delays.append(delay)
+
+    delays.sort()
+    return delays
