27c369a5f9f8be29a146c7276d65ee14ec831e76
chostler24 <connor.hostler@gmail.com>
2023-05-08 16:02:52 -0500

    added missed parantheses

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -20,6 +20,6 @@ def measure_time(n: int, max_delay: int) -> float:
     """measure_time function declaration"""
     start_time = perf_counter()
     asyncio.run(wait_n(n, max_delay))
-    end_time = perf_counter
+    end_time = perf_counter()
     total_time = end_time - start_time
     return total_time / n
