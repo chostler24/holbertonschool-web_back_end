f535171eec852117643bf92a9e3c874a644c1c25
chostler24 <connor.hostler@gmail.com>
2023-05-09 12:57:57 -0500

    Adding task 4

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Take the code from wait_n and alter it into a new
+function task_wait_n. The code is nearly identical
+to wait_n except task_wait_random is being called.
+"""
+import asyncio
+from typing import List
+from random import random
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """task_wait_n function declaration"""
+    delays = []
+    tasks = []
+    for i in range(n):
+        task = task_wait_random(max_delay)
+        tasks.append(task)
+
+    for task in tasks:
+        delay = await task
+        delays.append(delay)
+
+    return sorted(delays)
