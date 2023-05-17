bfc8f4ce14c0b044f9511703eea683109dc0b42a
chostler24 <connor.hostler@gmail.com>
2023-05-17 14:05:53 -0500

    Fixed for good (hopefully)

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -14,19 +14,15 @@ class MRUCache(BaseCaching):
 
     def put(self, key, item):
         """Put method assigns item value for key to dict"""
-        if key is None or item is None:
-            return
-
-        self.cache_data[key] = item
-
-        if key in self.queue:
-            self.queue.remove(key)
-        self.queue.append(key)
-
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            recent_key = self.queue.pop()
-            del self.cache_data[recent_key]
-            print("DISCARD: {}".format(recent_key))
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                discard = self.queue.pop()
+                del self.cache_data[discard]
+                print("DISCARD:", discard)
+            self.queue.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """Get method returns value in dict linked to key"""
