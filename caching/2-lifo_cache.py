332ef5976b989e849cf56b46a8e80e5ef5dc070e
chostler24 <connor.hostler@gmail.com>
2023-05-17 10:22:31 -0500

    minor tweaks to algo

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -14,15 +14,16 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """Put method assigns item value for key to dict"""
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
-        self.stack.append(key)
-
-        if len(self.stack) > self.MAX_ITEMS:
-            lastkey = self.stack.pop(0)
-            del self.cache_data[lastkey]
-            print("DISCARD: {}".format(lastkey))
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+            if len(self.stack) > self.MAX_ITEMS:
+                lastkey = self.stack.pop()
+                del self.cache_data[lastkey]
+                print("DISCARD: {}".format(lastkey))
+            self.stack.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
         """Get method returns value in dict linked to key"""
