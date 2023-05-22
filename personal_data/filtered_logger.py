31157353e6218a529ab28f0f200144a431838aed
chostler24 <connor.hostler@gmail.com>
2023-05-22 16:07:12 -0500

    Pushing README, gitignore, and task 0

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Write a function called filter_datum that returns the log message obfuscated
+"""
+import logging
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Arguments:
+    fields: list of strings representing fields to obfuscate
+    redaction: string representing by what field will be obfuscated
+    message: string represented by log line
+    separator: string represented by which character is separating fields
+
+    Function uses a regex to replace occurences of certain field values
+    Function uses re.sub to perform substitution with single regex
+    """
+    for field in fields:
+        regexStr = f"(?<={field}=).*?(?={separator})"
+        message = re.sub(regexStr, redaction, message)
+    return message
