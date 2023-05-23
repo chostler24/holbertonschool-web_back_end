2e611edb7cec50a4259cbb80fcd9b1c3d4cd545b
chostler24 <connor.hostler@gmail.com>
2023-05-23 11:38:05 -0500

    Adding task 5

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Encrypts a password using a one string argument"""
+import bcrypt
+
+
+def hash_password(password):
+    """Returns a salted, hashed password as a byte string"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(
+        password.encode('utf-8'), salt
+    )
+    return hashed_password
