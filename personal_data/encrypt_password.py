14e085eedcb852c8aa20963de6cf807bf5f08f07
chostler24 <connor.hostler@gmail.com>
2023-05-23 11:41:00 -0500

    Annotations

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,6 +3,6 @@
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password as a byte string"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
