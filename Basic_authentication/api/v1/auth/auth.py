851727534357ad7402dd08efe33a7f7e7c31a6c5
chostler24 <connor.hostler@gmail.com>
2023-05-24 14:38:06 -0500

    task 5

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -27,7 +27,12 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Public method returns None"""
-        return None
+        if request is None:
+            return None
+        if request.headers.get('Authorization') is None:
+            return None
+
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Public method returns None"""
