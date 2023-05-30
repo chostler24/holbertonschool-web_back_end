161b2ac8a3322a04413f7bb564bca5e7e64ae27e
chostler24 <connor.hostler@gmail.com>
2023-05-30 12:53:53 -0500

    documentation

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -9,6 +9,11 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """
+        Function create_session checks if user_id is str
+        Creates session_id and stores it in dict
+        Returns session_id
+        """
         if user_id is None or not isinstance(user_id, str):
             return None
 
