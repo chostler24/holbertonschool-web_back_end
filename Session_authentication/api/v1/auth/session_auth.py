12b4f0d32ab54470204c2df95e1a4bf24f561afd
chostler24 <connor.hostler@gmail.com>
2023-05-30 12:56:30 -0500

    task 3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -20,3 +20,10 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Function user_id_for_session_id returns user_id for session_id"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
