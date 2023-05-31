da78200259943b89f0671281a93eedd045cb9dd9
chostler24 <connor.hostler@gmail.com>
2023-05-31 15:54:40 -0500

    added task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,25 @@ from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 
 
+from db import DB
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a user """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f'User {email} already exists')
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+
+
 def _hash_password(password: str) -> str:
     """Password slinging hasher"""
     return hashpw(password.encode(), gensalt())
