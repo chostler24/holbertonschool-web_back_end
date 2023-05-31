53ec9ec7117222dfba2676d49da486bc9cf46868
chostler24 <connor.hostler@gmail.com>
2023-05-31 16:07:09 -0500

    task 10

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -30,6 +30,16 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """Creates sessionID """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> str:
     """Password slinging hasher"""
