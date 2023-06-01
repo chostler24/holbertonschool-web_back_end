7d5c8bb68da4173f235a834c59c2349f3344b042
chostler24 <connor.hostler@gmail.com>
2023-06-01 11:00:35 -0500

    task 17

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -61,3 +61,14 @@ def profile():
         return jsonify({"email": user.email})
     else:
         abort(403)
+
+
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """ Reset password token route """
+    try:
+        email = request.form.get("email")
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token})
+    except ValueError:
+        abort(403)
