83f0a10642803cd3ef8d6fcfd5d34d213d3176fb
chostler24 <connor.hostler@gmail.com>
2023-06-01 10:09:14 -0500

    task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -24,3 +24,17 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """ Login route """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email=email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
