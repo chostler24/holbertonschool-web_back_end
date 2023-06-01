25b09206fc55b9ca8aebbcf5f200ded534d9ff17
chostler24 <connor.hostler@gmail.com>
2023-06-01 11:02:31 -0500

    task 18

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -57,12 +57,22 @@ class Auth:
             pass
 
     def get_reset_password_token(self, email: str) -> str:
-        """ Reset a password token """
+        """Reset password function declaration"""
         try:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Password update function declaration"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
 
