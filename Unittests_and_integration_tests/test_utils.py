6693f030f09f7fdca8ec817acfcff2bec10dedff
chostler24 <connor.hostler@gmail.com>
2023-06-02 15:01:22 -0500

    task 1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -6,14 +6,24 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Class for utilizing access_nested_map function"""
+    """TestAccessNestedMap class"""
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Unittest module test_access_nested_map_exception"""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), "Key not found in nested map")
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """Unittest module"""
+        """Unittest module test_access_nested_map"""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected)
 
