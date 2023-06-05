9b292f6f8554c8ba25a5634bfb52e0e8727ccd05
chostler24 <connor.hostler@gmail.com>
2023-06-05 12:00:20 -0500

    task 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Module for class TestAccessNestedMap"""
 import unittest
+from unittest.mock import mock, patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,6 +26,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """Unittest module test_access_nested_map_exception"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson class declaration"""
+    @mock.patch('utils.requests.get')
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """get json method"""
+        mock_response = mock.Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 if __name__ == '__main__':
     unittest.main()
