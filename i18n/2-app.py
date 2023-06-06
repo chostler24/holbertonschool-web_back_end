2cdff3d859a11f84e9c899f629c185226907777d
chostler24 <connor.hostler@gmail.com>
2023-06-06 14:51:28 -0500

    typo in selector

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -23,7 +23,7 @@ def index():
     return render_template('1-index.html')
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale():
     """Function matches best supported language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
