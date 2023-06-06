9908a33817b9ab65de2545d72c877bac0dd09f67
chostler24 <connor.hostler@gmail.com>
2023-06-06 14:10:30 -0500

    initial commit and task 0

---
+++ i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module for basic Flask app"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """function returns rendering of index file"""
+    return render_template('o-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
