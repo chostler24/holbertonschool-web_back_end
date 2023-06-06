63e2e32f641a47a670c6c41b477aef7440a94c42
chostler24 <connor.hostler@gmail.com>
2023-06-06 14:34:50 -0500

    typo fix in app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """function returns rendering of index file"""
-    return render_template('o-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
