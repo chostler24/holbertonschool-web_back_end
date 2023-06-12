3032c2ffc4146e029af83ab88759000bac0ed95d
chostler24 <connor.hostler@gmail.com>
2023-06-12 14:38:47 -0500

    initial commit

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""module for Cache class"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache():
+    """declaration of class Cache"""
+    def __init__(self):
+        """initialization"""
+        self._redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """function takes data arg and returns a string"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
