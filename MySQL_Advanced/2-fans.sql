a8c4fd241c1583b9a39b8f9ca85e2b5c94b3ac03
chostler24 <connor.hostler@gmail.com>
2023-06-13 14:19:33 -0500

    fixed table addition method

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- SQL script ranks country origin of bands, ordered by number
 -- of non-unique fans
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
