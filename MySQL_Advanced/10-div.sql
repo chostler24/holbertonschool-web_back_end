70841d9503b99d3ea4350c6fd2f1e0426f6fec4e
chostler24 <connor.hostler@gmail.com>
2023-06-26 14:34:19 -0500

    task 10 again

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -6,12 +6,10 @@ CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
   IF b = 0 THEN
-    SET result = 0;
-  ELSE
-    SET result = a / b;
+    RETURN 0;
   END IF;
 
-  RETURN result;
+  RETURN a / b;
 END;
 
 //
