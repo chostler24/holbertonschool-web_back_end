449cb292244fe7446db80ea08c8b35c30ffd4050
chostler24 <connor.hostler@gmail.com>
2023-06-27 14:58:50 -0500

    changed if check

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
 
-  if (input) {
+  if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
 });
