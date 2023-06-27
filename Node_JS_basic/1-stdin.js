75306ee3b1398bcd63bd2ab2784288d6ef2a3a59
chostler24 <connor.hostler@gmail.com>
2023-06-27 14:25:17 -0500

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,10 +6,10 @@ process.stdin.on('readable', () => {
   const input = process.stdin.read();
 
   if (input) {
-    process.stdout.write(`Your name is: ${input}\n`);
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
 process.stdin.on('end', () => {
-  process.stdin.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
