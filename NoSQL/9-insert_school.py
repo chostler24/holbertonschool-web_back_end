c7c47363e7065e9820b7c7f8cf2ede8f9987f14c
chostler24 <connor.hostler@gmail.com>
2023-06-28 14:00:18 -0500

    task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,11 @@
+#!/user/bin/env python3
+"""
+9-insert_school module
+inserts new doc in collection
+"""
+from pymongo import MongoClient
+
+def insert_school(mongo_collection, **kwargs):
+    """inser_school function"""
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
