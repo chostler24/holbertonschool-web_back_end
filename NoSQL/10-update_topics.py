2cc13b77f43454a235c958a9ee252f68c21a7fe9
chostler24 <connor.hostler@gmail.com>
2023-06-28 14:04:30 -0500

    added docstring to function

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 
 
 def update_topics(mongo_collection, name, topics):
+    """update_topics function"""
     mongo_collection.update_many(
         { "name": name },
         { "$set": { "topics": topics } }
