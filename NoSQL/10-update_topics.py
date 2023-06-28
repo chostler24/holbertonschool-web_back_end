5a66fa5e99f302d1f09a365c4852749fa7fc31ae
chostler24 <connor.hostler@gmail.com>
2023-06-28 14:04:04 -0500

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+10-update_topics module
+changes all topics of school doc based on name
+"""
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    mongo_collection.update_many(
+        { "name": name },
+        { "$set": { "topics": topics } }
+    )
