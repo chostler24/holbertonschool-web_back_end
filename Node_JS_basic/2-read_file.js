e783c8103c0b854a8da216bdda622907775f89d4
chostler24 <connor.hostler@gmail.com>
2023-07-03 13:39:41 -0500

    task 2 passes lint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,34 +3,34 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
-    if (!fs.existsSync(path)) {
-        throw new Error('Cannot load the database');
-    }
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
 
-    const data = fs.readFileSync(path, 'utf8');
-    let lines = data.split('\n')
+  const data = fs.readFileSync(path, 'utf8');
+  let lines = data.split('\n');
 
-    // Calculate the number of students in each field
-    lines = lines.filter((line) => line != '').slice(1);
-    console.log(`Number of students: ${lines.length}`);
+  // Calculate the number of students in each field
+  lines = lines.filter((line) => line !== '').slice(1);
+  console.log(`Number of students: ${lines.length}`);
 
-    const field = lines.map((line) => line.split(',')[3]);
+  const field = lines.map((line) => line.split(',')[3]);
 
-    const eachField = [...new Set(field)];
+  const eachField = [...new Set(field)];
 
-    eachField.forEach((fieldName) => {
-        const studentsPerField = lines
-            .filter((line) => line.endsWith(fieldName))
-            .map((line) => {
-                const split = line.split(',');
-                return split[0];
-            });
-        console.log(
-            `Number of students in ${fieldName}: ${
-                studentsPerField.length
-            }. List: ${studentsPerField.join(', ')}`,
-        );
-    });
+  eachField.forEach((fieldName) => {
+    const studentsPerField = lines
+      .filter((line) => line.endsWith(fieldName))
+      .map((line) => {
+        const split = line.split(',');
+        return split[0];
+      });
+    console.log(
+      `Number of students in ${fieldName}: ${
+        studentsPerField.length
+      }. List: ${studentsPerField.join(', ')}`,
+    );
+  });
 };
 
 module.exports = countStudents;
