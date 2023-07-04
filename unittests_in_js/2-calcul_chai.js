699c73e4cc18172428b5fbaf0d527069e3e56fa9
chostler24 <connor.hostler@gmail.com>
2023-07-04 14:27:40 -0500

    tasl 2

---
+++ unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,23 @@
+// 2-calcul_chai.js module
+
+const calculateNumber = (type, a, b) => {
+    const aRound = Math.round(a);
+    const bRound = Math.round(b);
+    // let type = 'SUM' | 'SUBTRACT' | 'DIVIDE';
+    if (isNaN(a) || isNaN(b)) throw TypeError('Args a, b must be numbers');
+    if (typeof type !== 'string') throw TypeError('Arg type must be string');
+    // if (bRound === 0) throw Error('Error');
+    if (type === 'SUM') {
+        return aRound + bRound;
+    } else if (type === 'SUBTRACT') {
+        return aRound - bRound;
+    } else if (type === 'DIVIDE') {
+        if (bRound === 0) {
+            return 'Error';
+        } else {
+            return aRound / bRound;
+        };
+    };
+}
+
+module.exports = calculateNumber;
