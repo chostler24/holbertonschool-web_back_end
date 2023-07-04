01df7744b15311cf05e9047455bb68b249c50b2a
chostler24 <connor.hostler@gmail.com>
2023-07-04 13:22:00 -0500

    trying sumtin

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -3,7 +3,6 @@
 const calculateNumber = (a, b) => {
     const aRound = Math.round(a);
     const bRound = Math.round(b);
-    if (isNaN(a) || isNaN(b)) throw TypeError('Args must be numbers');
     return aRound + bRound;
 }
 
