fa99192f359e39d9e59031eda8097d67172f3d00
chostler24 <connor.hostler@gmail.com>
2023-07-04 13:50:48 -0500

    changed route for function req

--- unittests_in_js/1-calcul.js
+++ unittests_in_js/1-calcul.js
@@ -3,7 +3,7 @@
 const calculateNumber = (type, a, b) => {
     const aRound = Math.round(a);
     const bRound = Math.round(b);
-    let type = 'SUM' | 'SUBTRACT' | 'DIVIDE'
+    let type = 'SUM' | 'SUBTRACT' | 'DIVIDE';
     if (isNaN(a) || isNaN(b)) throw TypeError('Args a, b must be numbers');
     if (typeof type !== 'string') throw TypeError('Arg type must be string');
     // if (bRound === 0) throw Error('Error');
