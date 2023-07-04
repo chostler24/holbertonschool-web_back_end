b77d93bb620bd419631164eed619f36042c0d92c
chostler24 <connor.hostler@gmail.com>
2023-07-04 14:56:33 -0500

    task 5

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,28 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should log the correct message for totalAmount = 100 and totalShipping = 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleLogSpy.calledOnceWith('The total is: 120')).to.be.true;
+  });
+
+  it('should log the correct message for totalAmount = 10 and totalShipping = 10', () => {
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleLogSpy.calledOnceWith('The total is: 20')).to.be.true;
+  });
+});
