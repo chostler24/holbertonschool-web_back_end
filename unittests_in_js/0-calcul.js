84ca9ee29b9592a80633246e491198a5f4c5b41e
chostler24 <connor.hostler@gmail.com>
2023-07-04 12:55:09 -0500

    task 0 and README

---
+++ unittests_in_js/0-calcul.js
@@ -0,0 +1,9 @@
+// 0-calcul.js module
+
+const calculateNumber = (a, b) => {
+    const aRound = Math.round(a);
+    const bRound = Math.round(b);
+    return aRound + bRound;
+}
+
+module.exports = calculateNumber;
---
+++ unittests_in_js/0-main.js
@@ -0,0 +1,10 @@
+// test module for 0-calcul.js
+
+const calculateNumber = require('./0-calcul');
+
+// Example usage
+const a = 2.7;
+const b = 5.3;
+const sum = calculateNumber(a, b);
+
+console.log(`Rounded sum of ${a} and ${b} is: ${sum}`);
