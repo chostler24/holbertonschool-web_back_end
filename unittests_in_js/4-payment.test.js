ee46648c33bf95f744a1b76419142937ac795154
chostler24 <connor.hostler@gmail.com>
2023-07-04 14:50:28 -0500

    task 4

---
+++ unittests_in_js/4-payment.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./2-calcul_chai');
+
+describe('sendPaymentRequestToApi', () => {
+  let calculateNumberStub;
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with correct arguments', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberStub.calledOnceWith('SUM', 100, 20)).to.be.true;
+  });
+
+  it('should log the correct message to the console', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleLogSpy.calledOnceWith('The total is: 10')).to.be.true;
+  });
+});
