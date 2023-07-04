ac1633aa01f837955bd0f71546e971b7c52f285d
chostler24 <connor.hostler@gmail.com>
2023-07-04 13:07:26 -0500

    added type error check to calculateNumber

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,7 +1,10 @@
 // 0-calcul.js module
 
 const calculateNumber = (a, b) => {
-    return Math.round(a) + Math.round(b);
+    const aRound = Math.round(a);
+    const bRound = Math.round(b);
+    if (isNaN(a) || isNaN(b)) throw TypeError('Args must be numbers');
+    return aRound + bRound;
 }
 
 module.exports = calculateNumber;
