66f45a9225bb56907cd240f2179d77fe2fb12ba2
chostler24 <connor.hostler@gmail.com>
2023-07-05 10:25:46 -0500

    task 8

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,14 @@
+// testing suite for api.js module
+
+const { expect } = require('chai');
+const request = require('request');
+
+describe('API testing', () => {
+  it('Tests the GET / route', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
