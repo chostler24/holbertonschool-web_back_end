bd16b234df9f24aa88247ae69bbe80b509e99118
chostler24 <connor.hostler@gmail.com>
2023-07-05 10:37:06 -0500

    task 8 test suite again

--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,28 +1,15 @@
 // testing suite for api.js module
 
-const chai = require('chai');
-const expect = chai.expect;
+const request = require('request');
+const { expect } = require('chai');
 
-const app = require('./api');
 
 describe('Index Page', () => {
-  it('should return a 200 status code', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-
   it('should return the correct result', (done) => {
-    chai
-      .request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
+    request('http://localhost:7865', (error, response, res) => {
+      expect(response.statusCode).to.equal(200);
+      expect(res.text).to.equal('Welcome to the payment system');
+      done();
       });
   });
 });
